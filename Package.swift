892cba7ebbf42ca6594f7adb73461342564fa7c3
Sergey Di <sergey_di@icloud.com>
2019-07-15 12:02:39 +0300

    Migrate to PM4

--- Package.swift
+++ Package.swift
@@ -1,25 +1,15 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Socks",
+    products: [
+        .library(
+              name: "Socks",
+              targets: ["Socks"])
+      ],
     targets: [
-    	Target(name: "SocksCore"),
-    	Target(name: "Socks", dependencies: ["SocksCore"]),
-      //Target(name: "SocksCoreExampleTCPServer", dependencies: ["SocksCore"]),
-      //Target(name: "SocksCoreExampleTCPKeepAliveServer", dependencies: ["Socks"]),
-    	//Target(name: "SocksCoreExampleTCPClient", dependencies: ["SocksCore"]),
-    	//Target(name: "SocksExampleTCPServer", dependencies: ["Socks"]),
-    	//Target(name: "SocksExampleTCPClient", dependencies: ["Socks"]),
-    	//Target(name: "SocksExampleUDPClient", dependencies: ["Socks"]),
-    	//Target(name: "SocksExampleUDPServer", dependencies: ["Socks"]),
-    ],
-    exclude: [
-        "Sources/SocksCoreExampleTCPServer",
-        "Sources/SocksCoreExampleTCPKeepAliveServer",
-        "Sources/SocksCoreExampleTCPClient",
-        "Sources/SocksExampleTCPServer",
-        "Sources/SocksExampleTCPClient",
-        "Sources/SocksExampleUDPClient",
-        "Sources/SocksExampleUDPServer",
+    	.target(name: "SocksCore", path: "Sources/SocksCore"),
+    	.target(name: "Socks", dependencies: ["SocksCore"], path: "Sources/Socks")
     ]
 )
